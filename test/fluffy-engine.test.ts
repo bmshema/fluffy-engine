b01efb543b9a74acd95aff3429bad2a031466790
bmshema <92175742+bmshema@users.noreply.github>
2025-06-20 13:05:23 -0400

    Add project files

---
+++ test/fluffy-engine.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FluffyEngine from '../lib/fluffy-engine-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/fluffy-engine-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FluffyEngine.FluffyEngineStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
